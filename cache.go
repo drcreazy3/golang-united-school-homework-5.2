8cd925ac5e252ac63065c7fa3a26245762512d23
Sergei Sevkovich <drcreazy@gmail.com>
2022-06-27 11:01:56 +0300

    fixup

--- cache.go
+++ cache.go
@@ -1,23 +1,68 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
+
+const CLEAR_INTERVAL = 1
 
 type Cache struct {
+	elementMap             map[string]CacheElement
+	clearExpiredElementsAt time.Time
+}
+
+type CacheElement struct {
+	value    string
+	deadline time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	c := Cache{}
+	c.elementMap = make(map[string]CacheElement)
+	c.clearExpiredElementsAt = time.Now().Add(time.Second * time.Duration(CLEAR_INTERVAL))
+	return c
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c Cache) Get(key string) (string, bool) {
+	defer c.checkDeadline()
+	v := ""
+	ok := false
+	if cacheElement, ok := c.elementMap[key]; ok {
+		if cacheElement.deadline.IsZero() || time.Now().Before(cacheElement.deadline) {
+			return cacheElement.value, ok
+		}
+	}
 
+	return v, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	defer c.checkDeadline()
+	c.elementMap[key] = CacheElement{value: value}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	defer c.checkDeadline()
+	keys := make([]string, len(c.elementMap))
+	for k := range c.elementMap {
+		keys = append(keys, k)
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	defer c.checkDeadline()
+	c.elementMap[key] = CacheElement{value: value, deadline: deadline}
+}
+
+func (c Cache) checkDeadline() {
+	if time.Now().After(c.clearExpiredElementsAt) {
+		c.clearExpiredElementsAt = time.Now().Add(time.Second * time.Duration(CLEAR_INTERVAL))
+		for k, cacheElement := range c.elementMap {
+			if !cacheElement.deadline.IsZero() || time.Now().After(cacheElement.deadline) {
+				delete(c.elementMap, k)
+			}
+		}
+	}
 }
